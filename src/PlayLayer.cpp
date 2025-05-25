0c199150713fc9327bbc45cc1a2ddcf4f5bb141c
crewly <paolochristopharcilla@gmail.com>
2025-05-25 05:52:47 +0200

    ifdef instead of ifndef sdkflj

--- src/PlayLayer.cpp
+++ src/PlayLayer.cpp
@@ -55,7 +55,7 @@ class $modify(MyPlayLayer, PlayLayer) {
 
 	// Platformer GD
 	// Checkpoint activated
-	#ifndef GEODE_IS_ANDROID32 // temporarily disabled for android32
+	#ifdef GEODE_IS_ANDROID32 // temporarily disabled for android32
 	void checkpointActivated(CheckpointGameObject* p0) {
 		PlayLayer::checkpointActivated(p0);
 		if (!m_fields->m_comboBurst) {
